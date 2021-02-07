5d9fb29c9282ed31385229194e60ef1616f19c39
wdestein <bill_destein@intuit.com>
2021-02-07 10:58:25 -0800

    More work

--- src/App.js
+++ src/App.js
@@ -38,28 +38,23 @@ export default function BasicExample() {
             </li>
           </ul>
         </div>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
+        <div className="App-content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
       </div>
+      
+      <hr />
+
     </Router>
   );
 }
